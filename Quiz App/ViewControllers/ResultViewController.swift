7dcf9f8ac2fb4b09fac73c62d87bd96fe7195283
Vadim <103464053+Branios@users.noreply.github.com>
2022-06-08 02:43:20 +0300

    trying to make a short function to show results

--- Quiz App/ViewControllers/ResultViewController.swift
+++ Quiz App/ViewControllers/ResultViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 class ResultViewController: UIViewController {
     
     
+    @IBOutlet var resultAnimal: UILabel!
     @IBOutlet var animalDescription: UILabel!
+    
+    var answersInModel: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
 
     }
-
-
+    
+    private func showResult() {
+        let selectedAnimal = Dictionary(grouping: answersInModel, by: { $0.animal })
+        selectedAnimal.sorted(by: {$0 > $1})
+        
+            
+        }
 }
+    
+
+
+
